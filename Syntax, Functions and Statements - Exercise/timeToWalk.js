96432d33d660ceb42cb8fb6f2cf6238c94bbb8ee
HRISTINA STOYANOVA <viara68@abv.bg>
2023-04-11 12:37:08 +0300

    solved exercises assignment

---
+++ Syntax, Functions and Statements - Exercise/timeToWalk.js
@@ -0,0 +1,18 @@
+function timeToWalk(steps, footprintLength, speedKmH) {
+
+    let distance = steps * footprintLength;
+    let delay = Math.floor(distance / 500) * 60;
+    let speed = speedKmH * 1000 / (60 * 60);
+    let timeInSeconds = distance / speed + delay;
+
+    let hours = Math.floor(timeInSeconds / (60 * 60));
+    let minutes = Math.floor(timeInSeconds / 60);
+    let seconds = Math.ceil(timeInSeconds % 60);
+    
+    console.log(`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+
+}
+
+timeToWalk(4000, 0.60, 5);
+console.log('................');
+timeToWalk(2564, 0.70, 5.5);
