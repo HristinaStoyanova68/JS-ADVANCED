1bd1f4fd5786bba8056e676c6bea5840d0a761a9
HRISTINA STOYANOVA <viara68@abv.bg>
2023-04-28 10:44:32 +0300

    solved exercises assignment

---
+++ Arrays and Nested Arrays - Exercise/orbit.js
@@ -0,0 +1,26 @@
+function orbit(array) {
+
+    let [row, col, initialRowIndex, initialColIndex] = array;
+    let orbit = [];
+
+    for (let i = 0; i < row; i++) {
+        orbit.push([]);
+    }
+
+    for (let i = 0; i < row; i++) {
+
+        for (let j = 0; j < col; j++) {
+
+            orbit[i][j] = Math.max(Math.abs(i - initialRowIndex),
+                Math.abs(j - initialColIndex)) + 1;
+        }
+    }
+
+    orbit.forEach(x => console.log(x.join(' ')));
+}
+
+orbit([4, 4, 0, 0]);
+console.log('...........');
+orbit([5, 5, 2, 2]);
+console.log('...........');
+orbit([3, 3, 2, 2])
