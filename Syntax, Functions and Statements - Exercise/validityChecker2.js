14cc4342272df3d9330c050a65294156fb7814ea
HRISTINA STOYANOVA <viara68@abv.bg>
2023-05-15 14:18:33 +0300

    solved exercises assignment

---
+++ Syntax, Functions and Statements - Exercise/validityChecker2.js
@@ -0,0 +1,23 @@
+function validityChecker(x1, y1, x2, y2) {
+
+    const result = (x1, y1, x2, y2) => {
+
+        const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        let output = '';
+        if (Number.isInteger(distance)) {
+            output = `{${x1}, ${y1}} to {${x2}, ${y2}} is valid`;
+        } else {
+            output = `{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`;
+        }
+        console.log(output);
+    };
+
+    result(x1, y1, 0, 0);
+    result(x2, y2, 0, 0);
+    result(x1, y1, x2, y2);
+}
+
+validityChecker(3, 0, 0, 4);
+console.log('...............');
+validityChecker(2, 1, 1, 1);
