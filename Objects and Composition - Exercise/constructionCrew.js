9053b2f4a5206754dae00b97e2723d9bc8bbbd55
HRISTINA STOYANOVA <viara68@abv.bg>
2023-05-22 14:59:28 +0300

    solved exercises assignment

---
+++ Objects and Composition - Exercise/constructionCrew.js
@@ -0,0 +1,44 @@
+function contructionCrew(input) {
+    const worker = { ...input };
+
+    if (worker.dizziness === true) {
+
+        const neededWaterAmount = worker.weight * worker.experience * 0.1;
+        worker.levelOfHydrated += neededWaterAmount;
+        worker.dizziness = false;
+    }
+
+    return worker;
+}
+
+
+
+
+contructionCrew({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+});
+
+console.log('...........');
+
+contructionCrew({
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true
+});
+
+console.log('...........');
+
+
+contructionCrew({
+    weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false
+});
+
+
+
