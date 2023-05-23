a02493c3a4209fd41eb0837fd07d315824ac592c
HRISTINA STOYANOVA <viara68@abv.bg>
2023-05-24 01:55:25 +0300

    solved exercises assignment

---
+++ Objects and Composition - Exercise/storeCatalogue.js
@@ -0,0 +1,48 @@
+function storeCatalogue(array) {
+
+    const sortedArray = array.sort((a, b) => a.localeCompare(b));
+
+    const result = {};
+
+    for (let index = 0; index < sortedArray.length; index++) {
+        const element = sortedArray[index].split(' : ').join(': ');
+        const firstLetter = element[0];
+
+        if (!result[firstLetter]) {
+            result[firstLetter] = [];
+        }
+
+        result[firstLetter].push(`  ${element}`);
+    }
+
+    for (const [firstLetter, elements] of Object.entries(result)) {
+
+        console.log(firstLetter);
+        for (const element of elements) {
+
+            console.log(element);
+        }
+    }
+}
+
+storeCatalogue([
+    'Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']
+);
+console.log('..............');
+storeCatalogue([
+    'Banana : 2',
+    'Rubic\'s Cube : 5',
+    'Raspberry P : 4999',
+    'Rolex : 100000',
+    'Rollon : 10',
+    'Rali Car : 2000000',
+    'Pesho : 0.000001',
+    'Barrel : 10']
+);
---
+++ Objects and Composition - Exercise/townsToJSON.js
@@ -0,0 +1,42 @@
+function townsToJSON(input) {
+
+    const matrix = input
+        .map((row) =>
+            row
+                .trim()
+                .split("|")
+                .map((cell) => cell.trim())
+        )
+        .map((row) => row.filter((cell) => cell !== ""));
+
+    const headers = matrix[0];
+    const data = matrix.slice(1);
+
+    const result = data.map((row) => {
+        const tempDict = {};
+
+        headers.forEach((header, index) => {
+            const tryParseToNum = Number(row[index]);
+            tempDict[header] = isNaN(tryParseToNum)
+                ? row[index]
+                : Number(tryParseToNum.toFixed(2));
+        });
+
+        return tempDict;
+    });
+
+    console.log(JSON.stringify(result));
+}
+
+townsToJSON([
+    '| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |']
+);
+console.log('............');
+townsToJSON([
+    '| Town | Latitude | Longitude |',
+    '| Veliko Turnovo | 43.0757 | 25.6172 |',
+    '| Monatevideo | 34.50 | 56.11 |']
+
+);
