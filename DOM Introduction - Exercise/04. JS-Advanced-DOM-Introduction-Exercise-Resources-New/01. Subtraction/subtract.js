5fb5712f581ec9ea89e41b35aa2d342ba27db53b
HRISTINA STOYANOVA <viara68@abv.bg>
2023-05-30 15:52:47 +0300

    solved exercises assignment

--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,4 +1,9 @@
 function subtract() {
    
+    const num1 = Number(document.getElementById("firstNumber").value);
+            const num2 = Number(document.getElementById("secondNumber").value);
+
+            const result = num1 - num2;
+            document.getElementById("result").textContent = result;
     
 }
--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-  //TODO...
+
+  //constants
+  const CAMEL_CASE = "Camel Case";
+  const PASCAL_CASE = "Pascal Case";
+
+  //capture elements
+  const inputValueByLowerCase = document
+    .getElementById("text")
+    .value.toLowerCase();
+  const currCaseValue = document
+    .getElementById("naming-convention").value;
+    const result = document.getElementById("result");
+
+    //validate case
+    if (currCaseValue !== CAMEL_CASE && currCaseValue !== PASCAL_CASE) {
+      result.textContent = 'Error!';
+      return;
+    } 
+
+    const arrOfStrings = inputValueByLowerCase.split(' ');
+    let output = '';
+    let startingPoint = 0;
+
+    if (currCaseValue === CAMEL_CASE) {
+      output += arrOfStrings[0];
+      startingPoint = 1;
+    }
+
+    for (let i = startingPoint; i < arrOfStrings.length; i++) {
+      const currWord = arrOfStrings[i]
+      output += currWord[0].toUpperCase() + currWord.slice(1, currWord.length);
+
+      result.textContent = output;      
+    }
 }
+
