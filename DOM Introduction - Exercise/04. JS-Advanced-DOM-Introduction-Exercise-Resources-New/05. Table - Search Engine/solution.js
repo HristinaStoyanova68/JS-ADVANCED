afbe131076b929fcbec20712edd51d84ded9d17f
HRISTINA STOYANOVA <viara68@abv.bg>
2023-05-30 21:23:55 +0300

    solved exercises assignment

--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   const rows = document.querySelectorAll('tbody tr');
 
+   function onClick() {
+
+      const inputValueToLower = document
+         .getElementById('searchField')
+         .value.toLowerCase();
+
+      for (const row of rows) {
+         const rowTextToLower = row.textContent.toLowerCase();
+
+         if (rowTextToLower.includes(inputValueToLower)) {
+            row.setAttribute('class', 'select');
+         } else {
+            row.removeAttribute('class');
+         }
+      }
    }
 }
