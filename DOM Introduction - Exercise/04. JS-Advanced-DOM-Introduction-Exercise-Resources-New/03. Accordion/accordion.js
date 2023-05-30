4b6b209a3fb15f74703bb1b91451f068e8672ca3
HRISTINA STOYANOVA <viara68@abv.bg>
2023-05-30 16:25:00 +0300

    solved exercises assignment

--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,17 @@
 function toggle() {
-    console.log('TODO:...');
+    //constants
+    const MORE = 'More';
+    const LESS = 'Less';
+
+    //capture elements
+    const buttonSpan = document.querySelector('.button');
+    const divWrapperText = document.querySelector('#extra');
+
+    //toggle functionality
+    //change text of the button
+    buttonSpan.textContent = buttonSpan.textContent === MORE ? LESS : MORE;
+
+    //hide/show the text
+    divWrapperText.style.display = divWrapperText.style.display === 'none' || divWrapperText.style.display === '' ? 'block' : 'none';
+
 }
