ef81a127c383c2934f31ddd7cc9e5395cf13bf25
HRISTINA STOYANOVA <viara68@abv.bg>
2023-05-30 20:55:39 +0300

    solved exercises assignment

--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,27 @@
 function search() {
-   // TODO
+   //capture elements
+
+   const listOfTowns = document.querySelectorAll("#towns li");
+   const inputValue = document.querySelector("input").value;
+   const result = document.getElementById("result");
+
+   //business logic
+
+   let matches = 0;
+
+   for (const li of listOfTowns) {
+      const inputToLower = inputValue.toLowerCase();
+      const liTextToLower = li.textContent.toLowerCase();
+
+      if (liTextToLower.includes(inputToLower)) {
+         li.style.fontWeight = "bold";
+         li.style.textDecoration = "underline";
+         matches++;
+      } else {
+         li.style.fontWeight = "";
+         li.style.textDecoration = "";
+      }
+   }
+
+   result.textContent = `${matches} matches found`;
 }
