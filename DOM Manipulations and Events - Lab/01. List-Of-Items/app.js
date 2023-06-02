263397e30e314473713756d3121a6f83811bda8a
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-02 14:47:40 +0300

    solved exercises assignment

--- DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
+++ DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
@@ -1,11 +1,23 @@
 function addItem() {
+    //read input value
+    const text = document.getElementById('newItemText').value;
+
+    //create new li
+    const newLi = document.createElement('li');
+    newLi.textContent = text;
+
+    //obtain reference to list element
+    const list = document.getElementById('items');
     
-    let input = document.getElementById('newItemText');
-    let value = input.value;
+    //add new li to list
+    list.appendChild(newLi);
+    
+    // let input = document.getElementById('newItemText');
+    // let value = input.value;
 
-    let ul = document.getElementById('items');
-    let newLi = document.createElement('li');
+    // let ul = document.getElementById('items');
+    // let newLi = document.createElement('li');
 
-    newLi.textContent = value;
-    ul.appendChild(newLi);
+    // newLi.textContent = value;
+    // ul.appendChild(newLi);
 }
