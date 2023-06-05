ac7469725c4ee6a56afd67ede32a0c1255fec2f4
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-05 11:33:24 +0300

    solved exercises assignment

--- DOM Manipulations and Events - Lab/03. Add-Delete/app.js
+++ DOM Manipulations and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,22 @@
 function addItem() {
-    //TODO...
+    //read input value
+    const text = document.getElementById('newItemText').value;
+
+    //create new li
+    const newLi = document.createElement('li');
+    newLi.textContent = text;
+
+    const deleteBtn = document.createElement('a');
+    deleteBtn.href = '#';
+    deleteBtn.textContent = '[Delete]';
+
+    deleteBtn.addEventListener('click', () => newLi.remove());
+
+    newLi.appendChild(deleteBtn);
+
+    //obtain reference to list element
+    const list = document.getElementById('items');
+    
+    //add new li to list
+    list.appendChild(newLi);
 }
