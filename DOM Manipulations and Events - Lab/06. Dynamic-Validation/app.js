7ab8d9c7ebf961dc0ad5bfa04752fcd31f8ea8f6
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-06 11:02:24 +0300

    solved exercises assignment

--- DOM Manipulations and Events - Lab/06. Dynamic-Validation/app.js
+++ DOM Manipulations and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+    const input = document.getElementById('email');
+    
+    input.addEventListener('change', validationEmail);
+
+    function validationEmail() {
+
+        const inputValue = input.value;
+
+        const isValid = inputValue.match(/^[a-z]+@[a-z]+\.[a-z]+$/);
+
+        if (isValid) {
+            input.classList.remove('error');
+        } else {
+            input.classList.add('error');
+        }        
+    }
 }
--- DOM Manipulations and Events - Lab/07. Shopping-Cart/solution.js
+++ DOM Manipulations and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-   //TODO...
+    document.getElementsByClassName('shopping-cart')[0].addEventListener('click', onClick);
+    document.getElementsByClassName('checkout')[0].addEventListener('click', checkoutClick);
+
+
+    const cart = [];
+    const output = document.getElementsByTagName('textarea')[0];
+
+    function onClick(event) {
+        //debugger;
+        if (event.target.tagName == 'BUTTON' && event.target.classList.contains('add-product')) {
+
+            const product = event.target.parentNode.parentNode;
+            const name = product.querySelector('.product-title').textContent;
+            const price = Number(product.querySelector('.product-line-price').textContent);
+
+            cart.push({
+                name,
+                price,
+            });
+
+            output.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+        }
+    }
+
+    function checkoutClick() {
+        const addButtonsElement = Array.from(document.getElementsByClassName('add-product'));
+        addButtonsElement.forEach(button => button.setAttribute('disabled', true));
+        document.getElementsByClassName('checkout')[0].setAttribute('disabled', true);
+        const products = new Set();
+
+        cart.forEach(p => products.add(p.name));
+
+        const total = cart.reduce((t, c) => t + c.price, 0);
+        output.value += `You bought ${[...products.keys()].join(', ')} for ${total.toFixed(2)}.`;
+
+    }
 }
