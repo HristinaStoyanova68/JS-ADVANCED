b2b264cfa195dce0b9c1883aae398f5020768c3f
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-07 09:35:14 +0300

    solved exercises assignment

--- DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    //capture buttons
+    const [encodeBtn, decodeBtn] = [...document.querySelectorAll('main button')];
+    const [senderTextArea, receiverTextArea] = [...document.querySelectorAll('main textarea')];
+
+    //attach events
+
+    encodeBtn.addEventListener('click', encodeAndSend);
+    decodeBtn.addEventListener('click', decodeAndRead);
+
+    //helper functions
+
+    function transformText(text, callback) {
+        return text.split('').map(callback).join('');
+    }
+
+    function prevChar(char) {
+        return String.fromCharCode(char.charCodeAt(0) - 1);
+    }
+
+    function nextChar(char) {
+        return String.fromCharCode(char.charCodeAt(0) + 1);
+    }
+
+    //on click callbacks
+    function encodeAndSend(event) {
+
+        receiverTextArea.value = transformText(senderTextArea.value, nextChar);
+        senderTextArea.value = '';
+    }
+
+    function decodeAndRead(event) {
+        receiverTextArea.value = transformText(receiverTextArea.value, prevChar);
+    }
 }
