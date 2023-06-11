811fc8611f64a42c57c20323459fa8773e2c8a99
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-11 17:51:48 +0300

    solved exercises assignment

--- Advanced Functions - Exercises/07. Simple Calculator/app.js
+++ Advanced Functions - Exercises/07. Simple Calculator/app.js
@@ -1,7 +1,43 @@
 function calculator() {
-    // TODO:
+    // make dictionary
+    const html = {
+        selector1: '',
+        selector2: '',
+        output: '',
+    };
+    const operands = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+    }
+
+    //helper function 
+
+    function calculate(selector1Value, selector2Value, operand) {
+
+        return operands[operand](Number(selector1Value), Number(selector2Value));
+    }
+
+    //result
+
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            html.selector1 = document.querySelector(selector1);
+            // console.log(html.selector1);
+            html.selector2 = document.querySelector(selector2);
+            html.output = document.querySelector(resultSelector);
+        },
+        add: () => {
+
+            html.output.value = calculate(html.selector1.value, html.selector2.value, '+');
+        },
+        subtract: () => {
+            html.output.value = calculate(html.selector1.value, html.selector2.value, '-');
+        },
+    }
 }
 
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
 
 
 
