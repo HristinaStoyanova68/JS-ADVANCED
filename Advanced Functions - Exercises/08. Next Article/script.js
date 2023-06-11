ff378c5c3548350c3dbbd0142e975abe1dde40d2
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-11 19:16:08 +0300

    solved exercises assignment

--- Advanced Functions - Exercises/08. Next Article/script.js
+++ Advanced Functions - Exercises/08. Next Article/script.js
@@ -1,3 +1,20 @@
 function getArticleGenerator(articles) {
-    // TODO
+
+    //capture elements
+    const output = document.querySelector('#content');
+    const copyArticles = articles.slice();
+    //console.log(copyArticles);
+
+    //helper function
+
+    return function showNext() {
+
+        if (copyArticles[0]) {
+            const article = document.createElement('article');
+            article.textContent = copyArticles.shift();
+            output.appendChild(article);
+        }
+
+        return showNext;
+    };
 }
