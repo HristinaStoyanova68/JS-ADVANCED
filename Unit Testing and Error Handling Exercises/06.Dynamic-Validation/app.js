0ac68f9b545c5f0823b609f2149d3f02b4fc4bc1
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-14 14:05:47 +0300

    solved exercises assignment

--- Unit Testing and Error Handling Exercises/06.Dynamic-Validation/app.js
+++ Unit Testing and Error Handling Exercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,25 @@
 function validate() {
-    // TODO
+    //capture elements
+    const emailInput = document.getElementById('email');
+
+    //attach event
+    emailInput.addEventListener('change', onChange);
+
+    //callbacks
+    function onChange(event) {
+
+        const { target } = event;
+        const emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        // const isValid = emailRegex.test(target.value);
+        
+
+        // if (!isValid) {
+        //     target.classList.add('error');
+        // } else {
+        //     target.classList.remove('error');
+        // }
+
+        const operation = emailRegex.test(target.value) ? 'remove' : 'add';
+        target.classList[operation]('error');
+    }
 }
