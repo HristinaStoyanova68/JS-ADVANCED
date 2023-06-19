e3dfbe23efc2a092ac4f64c9455f5a02bf393384
HRISTINA STOYANOVA <viara68@abv.bg>
2023-06-19 15:07:46 +0300

    solved exercises assignment

---
+++ Prototypes and Inheritance Lab/person.js
@@ -0,0 +1,30 @@
+function createPerson(firstName, lastName) {
+
+    const result = {
+        firstName: firstName,
+        lastName: lastName,
+    }
+
+    Object.defineProperty(result, 'fullName', {
+        enumerable: true,
+        configurable: true,
+        get() {
+            return result.firstName + ' ' + result.lastName;
+        },
+        set(value) {
+            [result.firstName, result.lastName] = value.split(' ');
+        },
+    });
+
+    return result;
+}
+
+let person = createPerson("Peter", "Ivanov");
+console.log(person.fullName); //Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName); //George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName); //George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); //Nikola
+console.log(person.lastName); //Tesla
