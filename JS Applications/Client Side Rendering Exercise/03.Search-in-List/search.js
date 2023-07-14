359e27d5d851089e670053dade7b674643b914c9
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-07-15 02:21:54 +0300

    solved exercises assignments

--- JS Applications/Client Side Rendering Exercise/03.Search-in-List/search.js
+++ JS Applications/Client Side Rendering Exercise/03.Search-in-List/search.js
@@ -1,3 +1,36 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { towns } from './towns.js'
+
+const cardTemplate = html`
+   <ul>
+       ${towns.map((town) => {
+   return html`<li id="${town}">${town}</li>
+       `})}  
+      </ul>
+      `;
+
+const card = document.getElementById('towns');
+render(cardTemplate, card);
+
+document.querySelector('button').addEventListener('click', search);
+
 function search() {
-   // TODO
+   const text = document.getElementById('searchText').value;
+   const result = towns.filter(town => {
+      if (town.includes(text)) {
+         const match = document.getElementById(`${town}`);
+         match.setAttribute('class', 'active');
+         // document.getElementById(town).classList = 'active';
+         return town;
+      } else {
+         document.getElementById(town).classList.remove('active');
+      }
+   });
+
+   const resultHtml = document.getElementById('result');
+   resultHtml.textContent = `${result.length} matches found`;
 }
+
+
+
+
