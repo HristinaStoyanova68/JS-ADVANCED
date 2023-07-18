06bbc3c162560cae75646aeed2ada238fa849e3a
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-07-18 14:09:54 +0300

    solved exercise assignments

--- JS Applications/Routing Lab/Page-Demo/src/app.js
+++ JS Applications/Routing Lab/Page-Demo/src/app.js
@@ -1,6 +1,37 @@
 import page from '../node_modules/page/page.mjs';
 
-page('/', () => console.log('home path'));
-page('/catalog', () => console.log('catalog page'));
-page('/about', () => console.log('what about'));
+const main = document.querySelector('main');
+
+
+
+page('/home', homePage);
+page('/catalog', () => main.innerHTML = `<h1>Catalog</h1>
+    <ul>
+        <li><a href="/catalog/1">Product 1</a></li>
+        <li><a href="/catalog/2">Product 2</a></li>
+        <li><a href="/catalog/3">Product 3</a></li>
+    </ul>`);
+page('/catalog/:productId', productPage);
+page('/catalog/:category/:id', category);
+page('/about', () => main.innerHTML = 'what about');
+//page('*', () => main.innerHTML = '404 Not Found');
+
 page.start();
+
+function homePage(ctx) {
+    main.innerHTML = 'home path';
+}
+
+function productPage(ctx) {
+    main.innerHTML = 'product page' + ctx.params.productId;
+}
+
+function category(ctx) {
+const categoryName = ctx.params.category;
+const id = ctx.params.id;
+main.innerHTML = `<h1>Catalog</h1>
+<h2>${categoryName}</h2>
+<p>Viewing product: ${id}<p>
+`;
+
+}
