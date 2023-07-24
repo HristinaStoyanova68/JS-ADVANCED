aea4ef1cf41860c0b4b80330e2bb7e8a25a8e759
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-07-24 10:41:06 +0300

    solved exercise assignment

--- JS Applications/Exam_Preparation/Clear_Career/src/app.js
+++ JS Applications/Exam_Preparation/Clear_Career/src/app.js
@@ -15,6 +15,9 @@ import { detailsPage } from './views/details.js';
 import { createPage } from './views/create.js';
 import { editPage } from './views/edit.js';
 
+import * as api from './data/applications.js';
+window.api = api
+
 //TODO change render root depending on project HTML structure
 const root = document.getElementById('wrapper');
  
--- JS Applications/Exam_Preparation/Clear_Career/src/views/details.js
+++ JS Applications/Exam_Preparation/Clear_Career/src/views/details.js
@@ -1,4 +1,5 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
+import { getApplications, getUserApplication } from '../data/applications.js';
 import { deleteOffer, getById } from '../data/offers.js';
 import { getUserData } from '../util.js';
 
@@ -27,16 +28,20 @@ const detailsTemplate = (offer, onDelete) => html`
                 >
               </div>
             </div>
-            <!-- <p>Applications: <strong id="applications">1</strong></p> -->
+            <p>Applications: <strong id="applications">${offer.applications}</strong></p>
 
             <!--Edit and Delete are only for creator-->
-            ${offer.canEdit ? html`
+            ${offer.canEdit || offer.canApply ? html`
             <div id="action-buttons">
-              <a href="/catalog/${offer._id}/edit" id="edit-btn">Edit</a>
+                ${offer.canEdit ? html`
+                <a href="/catalog/${offer._id}/edit" id="edit-btn">Edit</a>
               <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>
+                ` : null}
+              ${offer.canApply ? html`
+              <a href="" id="apply-btn">Apply</a>` : null}
 
               <!--Bonus - Only for logged-in users ( not authors )-->
-              <!-- <a href="" id="apply-btn">Apply</a> -->
+              <!--  -->
             </div>` : null}
             
             
@@ -46,11 +51,22 @@ const detailsTemplate = (offer, onDelete) => html`
 
 export async function detailsPage(ctx) {
     const id = ctx.params.id;
-    const offer = await getById(id);
 
+    const requests = [
+        getById(id),
+        getApplications(id)
+    ];
     const userData = getUserData();
-    if (userData && userData._id == offer._ownerId) {
-        offer.canEdit = true;
+
+    if (userData) {
+        requests.push(getUserApplication(id, userData._id));
+    }  
+
+    const [offer, applications, hasApplied] = await Promise.all(requests);
+    offer.applications = applications;
+    if (userData) {
+        offer.canEdit = userData._id == offer._ownerId;
+        offer.canApply = hasApplied == 0;
     }
     ctx.render(detailsTemplate(offer, onDelete));
     async function onDelete() {
